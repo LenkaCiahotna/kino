675a0cad69067ccbe859161c8421edfc5baeb709
Lenka ciahotna <lenka.ciahotna@seznam.cz>
2023-04-15 22:26:39 +0200

    pokus

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
 
-<html>
-    <h1>Lenčino kino</h1>
-<body>
-    <form action="vypis.php" method="POST">
-<button name="druh" value="filmy" >filmy</button>
-<button  name="druh" value="saly">sály</button>
-<button  name="druh" value="promitani">promítání</button>
-
-    </form>
-</body>
-
-</html>
